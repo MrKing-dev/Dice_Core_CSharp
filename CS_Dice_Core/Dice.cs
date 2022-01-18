bcc90dd3145cdd3550977a824d4dd42ba9089d5f
MrKing-dev <91885325+MrKing-dev@users.noreply.github.com>
2022-01-17 20:59:09 -0500

    Add project files.

---
+++ CS_Dice_Core/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Dice_Core
+{
+    internal class Dice
+    {
+        public List<int> Roll(int sides, int quan)
+        {
+            Random rnd = new Random();
+            List<int> dice = new List<int>();
+            for(int i = 0; i < quan; i++)
+            {
+                dice.Add(rnd.Next(1, sides + 1));
+            }
+            return dice;
+        }
+    }
+}
---
+++ CS_Dice_Core/Program.cs
@@ -0,0 +1,56 @@
+﻿List<int> diceList = new List<int>();
+List<int> highlightList = new List<int>();
+int highlightLength = 0;
+//Input values
+int sides = 10;
+int quan = 10;
+bool highlight = false;
+bool explode = false;
+bool combine = false;
+
+
+CS_Dice_Core.Dice d = new CS_Dice_Core.Dice();
+
+
+if (explode == true)
+{
+    diceList = d.Roll(sides, quan);
+    List<int> high = diceList.FindAll(x => x == sides);
+    int highRoll = high.Count();
+    List<int> e = d.Roll(sides, highRoll);
+    diceList.AddRange(e);
+}
+else
+{
+    diceList = d.Roll(sides, quan);
+}
+
+if(highlight == true)
+{
+    if(sides == 10)
+    {
+        List<int> x = diceList.FindAll(n => n >= 8);
+        highlightList.AddRange(x);
+
+    }
+    else if(sides == 20)
+    {
+
+    }
+    else
+    {
+
+    }
+
+}
+else
+{
+
+}
+
+
+
+
+
+Console.ReadKey();
+
